a5e2642fc9fc99bea9039c96eb6da712d9b0954d
Yu Chen <yuchen.wayne@gmail.com>
2019-03-23 11:47:35 -0400

    submission

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,30 +1,33 @@
 #include "PID.h"
 
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
 
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
-
+void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+    this->p_error = 0.0;
+    this->i_error = 0.0;
+    this->d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+    return (-Kp * p_error) - (Ki * i_error) - (Kd * d_error);
 }
+
+
+
